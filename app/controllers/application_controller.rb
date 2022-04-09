8a6015d8837ed7bd09958bf548d8d0f0a67d9eca
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-04-09 23:10:09 +0000

    [Finish]4-11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  #devise利用の機能(ユーザ登録、ログイン認証など)が使われる前に実行されるメソッド
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def after_sign_in_path_for(resource)
+      post_images_path
+    end
+
+    def after_sign_out_path_for(resource)
+      about_path
+    end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
