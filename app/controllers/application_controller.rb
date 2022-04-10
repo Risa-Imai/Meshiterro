95f81bf4f16a0ea564c7d848f98d0ea23c28b24d
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-04-10 09:25:59 +0000

    [Finish]4-20

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_uuser!, except: [:top]
   #devise利用の機能(ユーザ登録、ログイン認証など)が使われる前に実行されるメソッド
   before_action :configure_permitted_parameters, if: :devise_controller?
 
---
+++ db/migrate/20220410070425_remove_post_image_from_favorites.rb
@@ -0,0 +1,5 @@
+class RemovePostImageFromFavorites < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :favorites, :post_image, :integer
+  end
+end
